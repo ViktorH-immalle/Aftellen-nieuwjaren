5d0aef1a869267d76440b09d8e36c765400c1676
Hendrickx Viktor <viktor.hendrickx@immalle.eu>
2017-11-17 10:32:57 +0100

    Add project files.

---
+++ Aantal dagen levend/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aantal_dagen_levend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
